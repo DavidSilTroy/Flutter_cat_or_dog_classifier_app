fb01bf590bb76471b800e4fa209ec3f3a1983f09
David Silva Troya <davidsiltroy2008@hotmail.com>
2023-01-15 15:38:08 +0100

    Problems with dependencies that are not updated, I won't suggest to use this meanwhile is not solved but for this course I have to...

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// @dart=2.9
+//remember to run with flutter run --no-sound-null-safety
+//Some dependecies has not the null safety update
+import 'package:catvsdog_detector_app/splashscreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,12 +9,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Cat and Dog Classifier',
+      debugShowCheckedModeBanner: false,
+      home: MySplash(),
     );
   }
 }
